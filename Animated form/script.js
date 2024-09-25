7b137f23bda2dd40416638a12b9a15cbec85e774
Vincent kiplangat <159619241+Masway2024@users.noreply.github.com>
2024-09-25 21:08:09 +0300

    Add files via upload

---
+++ Animated form/script.js
@@ -0,0 +1,8 @@
+const signInBtnLink = document.querySelector('.signinBtn-link');
+const signUpBtnLink = document.querySelector('.signupBtn-link');
+const wrapper = document.querySelector('.wrapper');
+
+signUpBtnLink.addEventListener('click',() => {wrapper.classList.toggle('active');});
+
+signUpBtnLink.addEventListener('click',() => {wrapper.classList.toggle('active');});
+
